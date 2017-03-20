1759ced5dcf510c16eaf5fa46c94f5fd3e7dc6e1
lanshuijuntuan <724598042@qq.com>
2017-03-21 00:00:52 +0800

    初始化插件模式目录

---
+++ MAFDemo/Contracts/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.AddIn.Contract;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    [AddinContract]
+    public interface IImageProcessorContract:IContract
+    {
+        byte[] ProcessImageBytes(byte[] pixels);
+    }
+}
