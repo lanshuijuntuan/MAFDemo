72cb8694e7c07c7e45c30b83f132d488caf7c897
zhq <724598042@qq.com>
2017-03-21 13:38:35 +0800

    upate: MAF demo框架

--- MAFDemo/HostSideViews/ImageProcessorHostView.cs
+++ MAFDemo/HostSideViews/ImageProcessorHostView.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace HostSideViews
 {
-    public class Class1
+    public abstract class ImageProcessorHostView 
     {
+        public abstract byte[] ProcessImageBytes(byte[] pixels);
     }
 }
