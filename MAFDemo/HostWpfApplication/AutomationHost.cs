975e44342e5e7cea98be80e756d0d2a28201a1e6
zhq <724598042@qq.com>
2017-03-22 09:30:17 +0800

    add:添加Host Application

---
+++ MAFDemo/HostWpfApplication/AutomationHost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace HostWpfApplication
+{
+    public class AutomationHost:HostSideViews.HostObject
+    {
+        private ProgressBar progressBar;
+
+        public AutomationHost(ProgressBar progressBar)
+        {
+            this.progressBar = progressBar;
+        }
+
+        public override void ReportProgress(float percent)
+        {
+            this.progressBar.Value = percent;
+        }
+
+    }
+}
---
+++ MAFDemo/HostWpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using HostSideViews;
+using System;
+using System.AddIn.Hosting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HostWpfApplication
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private AutomationHost mhost;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            string path = Environment.CurrentDirectory;
+            string[] warnings = AddInStore.Update(path);
+            foreach(string warning in warnings)
+            {
+                Console.WriteLine(warning);
+            }
+            IList<AddInToken> tokens = AddInStore.FindAddIns(typeof(ImageProcessorHostView), path);
+            lstAddIns.ItemsSource = tokens;
+            mhost = new AutomationHost(this.mProgress);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapSource source = (BitmapSource)(pic.Source);
+            int stride = source.PixelWidth * source.Format.BitsPerPixel / 8;
+            stride = stride + (stride % 4) * 4;
+            int arraySize = stride * source.PixelHeight * source.Format.BitsPerPixel / 8;
+            byte[] originalPixels = new byte[arraySize];
+            source.CopyPixels(originalPixels, stride, 0);
+
+            AddInToken token = (AddInToken)lstAddIns.SelectedItem;
+            if(token==null)
+            {
+                return;
+            }
+            HostSideViews.ImageProcessorHostView addin = token.Activate<HostSideViews.ImageProcessorHostView>(AddInSecurityLevel.Internet);
+            
+            byte[] changedPixels = addin.ProcessImageBytes(originalPixels);
+
+            BitmapSource newSource = BitmapSource.Create(source.PixelWidth, source.PixelHeight, source.DpiX, source.DpiY, source.Format, source.Palette, changedPixels, stride);
+
+            pic.Source = newSource;
+        }
+    }
+}
